236203cab05308bdd62cd24d8608bf48f0810cb4
semeh <mirisemehmiri@gmail.com>
2025-07-13 12:42:44 +0100

    logout button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,14 @@ import  Link  from 'next/link';
 import Image from 'next/image';
 import { isAuthenticated } from '@/lib/actions/auth.action';
 import { redirect } from 'next/navigation';
+
+
+const handleLogout = () => {
+  // Example: remove token and redirect to login
+  localStorage.removeItem("token"); 
+  // or use a signOut function if using Firebase, NextAuth, etc.
+  window.location.href = "/login"; 
+};
 const RootLayout = async ({ children }:{children:ReactNode}) => {
   const isUserAuthenticated = await isAuthenticated();
   if(!isUserAuthenticated) return redirect('/sign-in');
@@ -13,7 +21,12 @@ const RootLayout = async ({ children }:{children:ReactNode}) => {
           <Image src= "/logo.svg" alt='logo' width={38} height={32}/>
           <h2 className='text-primary-100'>PrepWise</h2>
         </Link>
-
+        <button 
+    onClick={handleLogout} 
+    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+  >
+    Log Out
+  </button>
       </nav>
       {children}
     </div>
