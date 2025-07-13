92008584aa68f102867bba12982902864b188a5e
semeh <mirisemehmiri@gmail.com>
2025-07-13 13:41:37 +0100

    bye2

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,18 @@
+import Agent from '@/components/Agent';
+import { getCurrentUser } from '@/lib/actions/auth.action';
 
-import Agent from '@/components/Agent'
-import { getCurrentUser } from '@/lib/actions/auth.action'
-const page = async () => {
+// ⬅️ This line is essential to avoid build-time errors
+export const dynamic = 'force-dynamic';
+
+const Page = async () => {
   const user = await getCurrentUser();
+
   return (
     <>
-    <h3>Interview Generation</h3>
-    <Agent userName={user?.name} userId={user?.id} type="generate"/>
+      <h3>Interview Generation</h3>
+      <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
