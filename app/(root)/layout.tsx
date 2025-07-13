b85e971a9b7e191aa6b9fff8650d0e44675016d9
semeh <mirisemehmiri@gmail.com>
2025-07-13 13:35:22 +0100

    bye

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -32,7 +32,7 @@ const RootLayout = async ({ children }:{children:ReactNode}) => {
         onClick={handleLogout}
         className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
       >
-        Log Out
+        Bye
       </button>
       </nav>
       {children}
