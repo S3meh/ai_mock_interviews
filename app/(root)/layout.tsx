b51a1ecd96f05af8d9fe579fc438f5417a4c387e
semeh <mirisemehmiri@gmail.com>
2025-07-13 12:52:54 +0100

    logout button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,17 +3,23 @@ import  Link  from 'next/link';
 import Image from 'next/image';
 import { isAuthenticated } from '@/lib/actions/auth.action';
 import { redirect } from 'next/navigation';
+import { auth } from '@/firebase/client';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 
-const handleLogout = () => {
-  // Example: remove token and redirect to login
-  localStorage.removeItem("token"); 
-  // or use a signOut function if using Firebase, NextAuth, etc.
-  window.location.href = "/login"; 
-};
 const RootLayout = async ({ children }:{children:ReactNode}) => {
+  const router = useRouter();
   const isUserAuthenticated = await isAuthenticated();
   if(!isUserAuthenticated) return redirect('/sign-in');
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      router.push('/login');
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
   return (
     <div className='root-layout'>
       <nav>
@@ -21,12 +27,12 @@ const RootLayout = async ({ children }:{children:ReactNode}) => {
           <Image src= "/logo.svg" alt='logo' width={38} height={32}/>
           <h2 className='text-primary-100'>PrepWise</h2>
         </Link>
-        <button 
-    onClick={handleLogout} 
-    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
-  >
-    Log Out
-  </button>
+        <button
+        onClick={handleLogout}
+        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+      >
+        Log Out
+      </button>
       </nav>
       {children}
     </div>
